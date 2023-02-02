bb32ac5722a690ed184f92f0da3b7ec18e8363d7
pullareddyt <124253246+pullareddyt@users.noreply.github.com>
2023-02-02 11:43:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.text('i want to eat pizza')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
