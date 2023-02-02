0c2295d938c0cc7e1d3b7581739292cca3cac077
pullareddyt <124253246+pullareddyt@users.noreply.github.com>
2023-02-02 11:48:07 +0100

    Update streamlit_app.py
    my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.header('🐔 Breakfast Menu')
 streamlit.text(' 🥑 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥣 🥗 🐔 🥑🍞 Hard-Boiled Free-Range Egg')
+
+iport pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
